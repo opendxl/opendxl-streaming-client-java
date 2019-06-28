abcc4c81615047d480ad951cbe3b3e655d17aac3
Hugo Villalba <hugo.villalba@gmail.com>
2019-06-28 10:34:20 -0700

    Move samples to sample folder. Add script to run samples.

--- sample/src/sample/ConsumeRecordsWithUserPass.java
+++ sample/src/sample/ConsumeRecordsWithUserPass.java
@@ -2,11 +2,11 @@
  * Copyright (c) 2019 McAfee, LLC - All Rights Reserved.                     *
  *---------------------------------------------------------------------------*/
 
-package com.opendxl.streaming.sample;
+package sample;
 
 import com.opendxl.streaming.client.Channel;
 import com.opendxl.streaming.client.HttpProxySettings;
-import com.opendxl.streaming.client.auth.ChannelAuthToken;
+import com.opendxl.streaming.client.auth.ChannelAuthUserPass;
 import com.opendxl.streaming.client.ConsumerRecordProcessor;
 import com.opendxl.streaming.client.entity.ConsumerRecords;
 import com.opendxl.streaming.client.exception.PermanentError;
@@ -19,48 +19,31 @@ import java.util.Properties;
 
 /**
  * This example uses the opendxl-streaming-java-client to consume records from a Databus topic. It instantiates
- * a ChannelAuthToken object, a Channel object and a ConsumerRecordProcessor object. Then, the Channel run method is
+ * a ChannelAuthUserPass object, a Channel object and a ConsumerRecordProcessor object. Then, the Channel run method is
  * invoked and Channel starts to indefinitely consume records and to deliver them to ConsumerRecordProcessor which
  * finally prints them out. To quit this sample program, press CTRL+C or execute kill to call Channel destroy method
  * which will stop the Channel and gracefully exit.
  */
-public class ConsumeRecordsWithToken {
+public class ConsumeRecordsWithUserPass {
 
-    private ConsumeRecordsWithToken() { }
+    private ConsumeRecordsWithUserPass() { }
 
     public static void main(String[] args) {
 
         String channelUrl = "http://127.0.0.1:50080";
-        String token = "secret";
+        String channelUsername = "me";
+        String channelPassword = "secret";
         String channelConsumerGroup = "sample_consumer_group";
         List<String> channelTopicSubscriptions = Arrays.asList("case-mgmt-events",
                 "my-topic",
                 "topic-abc123",
                 "topic1");
 
-        // CA bundle certificate chain of trusted CAs provided as a string. The CA
+        // Path to a CA bundle file containing certificates of trusted CAs. The CA
         // bundle is used to validate that the certificate of the server being connected
         // to was signed by a valid authority. If set to an empty string, the server
         // certificate is not validated.
-        String verifyCertificateBundle = "-----BEGIN CERTIFICATE-----"
-                + "MIIDBzCCAe+gAwIBAgIJALteQYzVdTj3MA0GCSqGSIb3DQEBBQUAMBoxGDAWBgNV"
-                + "BAMMD3d3dy5leGFtcGxlLmNvbTAeFw0xOTA0MjIxNTI2MjZaFw0yOTA0MTkxNTI2"
-                + "MjZaMBoxGDAWBgNVBAMMD3d3dy5leGFtcGxlLmNvbTCCASIwDQYJKoZIhvcNAQEB"
-                + "BQADggEPADCCAQoCggEBAMHv/jBHmUI6s2FhDdw4I7I9RTU3yvpkXM1e/5ISfBwe"
-                + "18gkeml7q9t9eLpPc08W0akYn/SySeT0TEvw6w8mpCfEefe+RHg7f6taAzzMwtei"
-                + "bt98VSdrckQh2DfL+Dp47BeP/XsHh80V4rschYbK/RCt6tMARcR5VRoC3VETKGqH"
-                + "tGTgUjLrpsCqsPTQuSLaST8brLp0KBVS1T39ltB6UFLdmw3WxiuuHvy9Tk5KLuFv"
-                + "SjfR6zPP/b9BsnYw35rceEB/+bh3KGCnTS6hO1Qbt3sAolOc6Y8VuDAQRZfsD7m5"
-                + "8hrsvT/7VRBr0RoWUSYTZJRXrPUUmjP3CMJkfeXOauUCAwEAAaNQME4wHQYDVR0O"
-                + "BBYEFN6UJ/tpppQTRvb5zo+6nnPGfJoXMB8GA1UdIwQYMBaAFN6UJ/tpppQTRvb5"
-                + "zo+6nnPGfJoXMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADggEBAB635aKg"
-                + "mAifM5P/T9Xt8tFpIfVWGRc9dbdWsce1/IMoMUDwQuGpmvdKfY68FVN4niKC/HeB"
-                + "J4OBflvM7by8KBC28N/g8It/rqOCU14JFyCcYQPpPj7uTFLwiGuraGnnCGX+GYW3"
-                + "bGSCAVnJvH0gb0kWFTJwdK1dBUsMrRwHDhkrYLe8Z6NzT39VA3hI9cedbzIsfAJf"
-                + "pRBkBaMQyB6u15NHnqKy57zpekuoChU8snWLu7G8E6coMW1AlMGuNiZZqX3XCvAd"
-                + "8gc45ashE41QRpGz9fh3FfUJIq1BBoIjvJahzIPLVfvfDhTwpBHZ+PJkBcsUUgcf"
-                + "lHTRe1CZks4JfS8="
-                + "-----END CERTIFICATE-----";
+        String verifyCertificateBundle = "/tmp/certificates.crt";
 
         Properties extraConfigs = new Properties();
         extraConfigs.put("enable.auto.commit", false);
@@ -95,19 +78,36 @@ public class ConsumeRecordsWithToken {
          */
         extraConfigs.put("session.timeout.ms", 15000);
 
+        /**
+         * Http Proxy settings. If no proxy is required to connect to specified channelUrl, then set httpProxySettings
+         * to null
+         */
+        HttpProxySettings httpProxySettings;
+        try {
+            httpProxySettings = new HttpProxySettings(true,
+                    "10.20.30.40",
+                    8080,
+                    "me",
+                    "secret");
+        } catch (final PermanentError e) {
+            System.out.println("Error occurred: " + e.getMessage() + " No http proxy will be used.");
+            httpProxySettings = null;
+        }
+
         try (Channel channel = new Channel(channelUrl,
-                new ChannelAuthToken(token),
+                new ChannelAuthUserPass(channelUrl,
+                        channelUsername,
+                        channelPassword,
+                        null,
+                        verifyCertificateBundle,
+                        httpProxySettings),
                 channelConsumerGroup,
                 null,
                 null,
                 true,
                 verifyCertificateBundle,
                 extraConfigs,
-                new HttpProxySettings(true,
-                        "10.20.30.40",
-                        8080,
-                        "me",
-                        "secret"))) {
+                httpProxySettings)) {
 
             // Setup shutdown hook to call stop when program is terminated
             Runtime.getRuntime().addShutdownHook(
@@ -155,8 +155,7 @@ public class ConsumeRecordsWithToken {
             };
 
             // Consume records indefinitely
-            final int consumePollTimeoutMs = 500;
-            channel.run(consumerRecordCallback, channelTopicSubscriptions, consumePollTimeoutMs);
+            channel.run(consumerRecordCallback, channelTopicSubscriptions);
 
         } catch (final PermanentError | StopError | TemporaryError e) {
 
